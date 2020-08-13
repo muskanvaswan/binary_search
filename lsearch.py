fcd9cafe42d815f85d949d47216248d6af5fa67e
Muskan Vaswan <42072661+muskanvaswan@users.noreply.github.com>
2020-08-14 01:30:24 +0530

    Add files via upload

---
+++ lsearch.py
@@ -0,0 +1,12 @@
+list = [int(item) for item in input("Enter list (seperated by space): ").split()]
+num = int(input("Enter element to be found: "))
+
+def lsearch(l, n):
+    flag = "not found"
+    for i, j in enumerate(l):
+        if j==n:
+            flag = f"found at {i}"
+
+    return flag
+
+print(lsearch(list, num))
